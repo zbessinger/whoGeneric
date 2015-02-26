867c5c3ebad811e3f1bdec69e7bb6ef002eb2799
Zach Bessinger <zach.bessinger@gmail.com>
2015-02-26 11:35:19 -0500

    Bug fix for MATLAB r2014a; Added a script to demo in one click

---
+++ compileRunDemo.m
@@ -0,0 +1,3 @@
+compileWHOEfficient
+initWorkspaceWHOGeneric
+demoWhitenedHOG
--- compileWHOEfficient.m
+++ compileWHOEfficient.m
@@ -1,18 +1,18 @@
 % Compile mex functions
 %
 % feature extraction
-mex -O features/featuresHOGGrayScale.cc -o features/featuresHOGGrayScale
-mex -O features/featuresHOGColor.cc     -o features/featuresHOGColor
-mex -O features/featuresHOGorig.cc      -o features/featuresHOGorig
+mex -O features/featuresHOGGrayScale.cc -output features/featuresHOGGrayScale
+mex -O features/featuresHOGColor.cc     -output features/featuresHOGColor
+mex -O features/featuresHOGorig.cc      -output features/featuresHOGorig
 %
 % image resizing ( by scalar factors )
-mex -O features/resizeGrayScale.cc      -o features/resizeGrayScale
-mex -O features/resizeColor.cc          -o features/resizeColor
+mex -O features/resizeGrayScale.cc      -output features/resizeGrayScale
+mex -O features/resizeColor.cc          -output features/resizeColor
 %
 % image shrinking ( always by factor of 2 )
-mex -O features/reduceGrayScale.cc      -o features/reduceGrayScale
-mex -O features/reduceColor.cc          -o features/reduceColor
+mex -O features/reduceGrayScale.cc      -output features/reduceGrayScale
+mex -O features/reduceColor.cc          -output features/reduceColor
 %
 % discrete convolutions
-mex -O features/fconv2D.cc              -o features/fconv2D
-mex -O features/fconv3D.cc              -o features/fconv3D
+mex -O features/fconv2D.cc              -output features/fconv2D
+mex -O features/fconv3D.cc              -output features/fconv3D
